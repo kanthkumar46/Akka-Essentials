8f9e12a8eb4b755143007bf4ee4078c9c2a2608e
kanthkumar <kkd6428@rit.edu>
2016-04-15 04:13:49 -0400

    Removed Scala Programs

--- AkkaUnitTest/src/main/scala/org/akka/essentials/unittest/example/BoomActor.scala
+++
@@ -1,9 +0,0 @@
-package org.akka.essentials.unittest.example
-import akka.actor.Actor
-
-class BoomActor extends Actor {
-  def receive: Receive = {
-    case message: String => throw new IllegalArgumentException("boom!")
-    case message: Integer => throw new NullPointerException("caput")
-  }
-}
--- AkkaUnitTest/src/main/scala/org/akka/essentials/unittest/example/EchoActor.scala
+++
@@ -1,9 +0,0 @@
-package org.akka.essentials.unittest.example
-import akka.actor.Actor
-
-class EchoActor extends Actor {
-
-  def receive: Receive = {
-    case message => sender ! message
-  }
-}
